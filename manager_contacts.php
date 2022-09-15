0fe84376997c696c31594ef849ae354ca766b839
Diego Souza <50335837+dscostat7@users.noreply.github.com>
2022-09-15 09:41:09 -0300

    Create manager_contacts.php

---
+++ manager_contacts.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: Manager Contacts
+* Plugin URI: https://github.com/dscostat7/manager_contacts
+* Description: Plugin for management many contacts
+* Version: 1.0.0
+* Author: Diego Souza
+* Author URI: https://beacons.ai/dscostat7
+* License: GPL2
+*/
